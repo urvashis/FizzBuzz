63360140c5c30715fe0f666eafb7a5a96d471825
urvashis <urvashi8@gmail.com>
2016-10-06 11:03:32 +0100

    Update Program.cs

--- dotnet/FizzBuzz/FizzBuzz/Program.cs
+++ dotnet/FizzBuzz/FizzBuzz/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace FizzBuzz
@@ -7,6 +7,27 @@ namespace FizzBuzz
   {
     static void Main(string[] args)
     {
+        Console.WriteLine("Please enter a number");
+        var userInput = Console.ReadLine();
+        int number;
+        if (Int32.TryParse(userInput, out number))
+            Console.WriteLine(GetFizzBuzz(number));
+        else
+            Console.WriteLine("Please enter a valid integer number (Int32)");
+    }
+
+    public static string GetFizzBuzz(int number)
+    {
+        var multipleOf3 = number % 3 == 0;
+        var multipleOf5 = number % 5 == 0;
+
+        if (multipleOf3 && multipleOf5)
+            return "Fizzbuzz";
+        if (multipleOf3)
+            return "Fizz";
+        if (multipleOf5)
+            return "Buzz";
+        return number.ToString();
     }
   }
 }
